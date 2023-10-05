89d876f8387f0fd8ac77d3916b786e454244e9c2
Wilson Franca <wilson.franca.92@gmail.com>
2023-10-05 07:28:15 -0700

    feat: added route to update information of a user

--- src/routes.js
+++ src/routes.js
@@ -18,7 +18,7 @@ export const routes = [
     method: 'POST',
     path: buildRoutePath('/users'),
     handler: (req, res) => {
-      const {name, email} = req.body
+      const { name, email } = req.body
 
       const user = {
         id: randomUUID(),
@@ -38,6 +38,18 @@ export const routes = [
       const { id } = req.params 
 
       database.delete('users', id)
+
+      return res.writeHead(204).end()
+    }
+  },
+  {
+    method: 'PUT',
+    path: buildRoutePath('/users/:id'),
+    handler: (req, res) => {
+      const { id } = req.params 
+      const { name, email } = req.body
+
+      database.update('users', id, {name, email})
 
       return res.writeHead(204).end()
     }
